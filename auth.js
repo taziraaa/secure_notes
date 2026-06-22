663677df6b8e15389a7e30eca6a8e3c40e075afd
ajupi <juantazira@gmail.com>
2026-06-22 21:31:16 +0700

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcryptjs');
-const db = require('../config/db');
+const db = require('../db');
 
 // Halaman login
 router.get('/login', (req, res) => {
